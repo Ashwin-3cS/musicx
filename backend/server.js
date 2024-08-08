7e387bb09a58c0afae8bc414e09c8a312f661eee
KaushikKC <kccreations1704@gmail.com>
2024-08-08 14:05:22 +0530

    cors error updated

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,28 @@
 import express from "express";
 import dotenv from "dotenv";
-
 import authRoutes from "./routes/auth.routes.js";
 import mongooseConnection from "./db/connectToMongoDb.js";
+import cors from "cors";
 
-dotenv.config(); 
+dotenv.config();
 
 const app = express();
 
-app.use(express.json()); 
+app.use(cors());
+
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    mongooseConnection();
-    console.log(`Server is running on port ${PORT}`);
+  mongooseConnection();
+  console.log(`Server is running on port ${PORT}`);
 });
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
